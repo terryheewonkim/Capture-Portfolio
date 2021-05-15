1dd64128ccf479ca9b972fa79c5cb3cde4be5667
Terry Kim <terrykim025@gmail.com>
2021-05-15 16:37:14 +0900

    add Services and FAQ sections

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,17 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    // Fragment <> : basically an empty div so there is no rendering of an actual div
+    <>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
+    </>
+  );
 };
 
 export default AboutUs;
