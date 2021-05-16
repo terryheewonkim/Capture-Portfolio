c025440dbc7bad782340963127be4b2d2956bf7a
Terry Kim <terrykim025@gmail.com>
2021-05-16 17:02:57 +0900

    Style ServicesSection

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -9,6 +9,7 @@ const GlobalStyle = createGlobalStyle`
 
 body {
   background: #1b1b1b;
+  font-family: "Inter", sans-serif;
 }
 
 button {
@@ -20,6 +21,7 @@ button {
   background: transparent;
   color: #fff;
   transition: all 0.5s ease;
+  font-family: "Inter", sans-serif;
   &:hover {
     background-color: #23d997;
     color: #fff;
