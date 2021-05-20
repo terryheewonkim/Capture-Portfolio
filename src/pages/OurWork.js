45a14f4f2f81450696939d311b864b24cf4f397e
Terry Kim <terrykim025@gmail.com>
2021-05-20 17:02:53 +0900

    fix animations

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -16,6 +16,7 @@ import {
   sliderContainer,
 } from "../animation";
 import { useScroll } from "../components/useScroll";
+import ScrollTop from "../components/ScrollTop";
 
 const OurWork = () => {
   const [element, controls] = useScroll();
@@ -68,6 +69,7 @@ const OurWork = () => {
           <img src={goodtimes} alt="good times" />
         </Link>
       </Movie>
+      <ScrollTop />
     </Work>
   );
 };
